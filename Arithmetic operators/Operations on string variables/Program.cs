a211726492f7219d71d85c41596ada5244e11f69
EgorRosliakov <egorka.030890@gmail.com>
2023-09-28 17:18:36 +0300

    28.09.23

---
+++ Arithmetic operators/Operations on string variables/Program.cs
@@ -0,0 +1,9 @@
+﻿/*Напишите приложение, в котором программа получает и сохраняет в отдельные
+переменные от пользователя сначала его имя, потом его фамилию, а затем выводит на
+экран строку по типу “Hello <Имя_пользователя> <Фамилия пользователя>! It`s a god
+day today!” Данный текст должен быть составлен одной строкой, при этом после
+первого предложения должен выполняться перенос строки на новую с использованием
+escape-последовательности.*/
+
+
+Console.Write("Enter you name: ");string customerName = Console.ReadLine();Console.Write("Enter you surname: ");string customerSurname = Console.ReadLine();Console.WriteLine($"Hello {customerName} {customerSurname}!\nIt`s a god day today!");
---
+++ Arithmetic operators/String variables/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите приложение, которое сначала примет от пользователя в 5 строковых
+переменных 5 значений, а затем выведет их на экран по одному символу на строку. При
+вводе по одной букве слова“Hello” вы должны получить результат вывода на экран
+значений переменных:
+H
+e
+l
+l
+o*/
+
+char firstLetter = Convert.ToChar(Console.ReadLine());
+char secondLetter = Convert.ToChar(Console.ReadLine());
+char thirdLetter = Convert.ToChar(Console.ReadLine());
+char quadrupleLetter = Convert.ToChar(Console.ReadLine());
+char fifthLetter = Convert.ToChar(Console.ReadLine());
+Console.WriteLine(firstLetter);
+Console.WriteLine(secondLetter);
+Console.WriteLine(thirdLetter);
+Console.WriteLine(quadrupleLetter);
+Console.WriteLine(fifthLetter);
