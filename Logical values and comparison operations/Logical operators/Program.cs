d278e3e9e6101d3bafd0df1bb4d945401f470d3f
EgorRosliakov <egorka.030890@gmail.com>
2023-10-04 20:01:53 +0300

    some changes to previous homework

--- Logical values and comparison operations/Logical operators/Program.cs
+++ Logical values and comparison operations/Logical operators/Program.cs
@@ -17,21 +17,34 @@
 Console.WriteLine("Enter how many years you have been working: ");
 int experience = Convert.ToInt32(Console.ReadLine());
 
-bool bonus1 = experience >= 0 && experience > 5;
-bool bonus2 = experience >= 5 && experience > 10;
-bool bonus3 = experience >= 10 && experience > 15;
-bool bonus4 = experience >= 15 && experience > 20;
-bool bonus5 = experience >= 20 && experience > 25;
-bool bonus6 = experience >= 25;
+bool bonus1 = experience >= 0 && experience <= 5;
+bool bonus2 = experience > 5 && experience <= 10;
+bool bonus3 = experience > 10 && experience <= 15;
+bool bonus4 = experience > 15 && experience <= 20;
+bool bonus5 = experience > 20 && experience <= 25;
+bool bonus6 = experience > 25;
 
-const int percentTen = 10, percentFifteen = 15,
-percentTwentyFive = 25,  percentThirtyFive = 35,
- percentFortyFive = 45, percentFifty = 50;
+const int PERCENT_TEN = 10, PERCENT_FIFTEEN = 15, 
+PERCENT_TWENTY_FIVE = 25, PERCENT_THIRTY_FIVE = 35,
+ PERCENT_FORTY_FIVE = 45, PERCENT_FIFTY = 50;
 
-/*  HACK   bool bonusPercent = (bonus1 == percentTen)  (bonus2 == percentFifteen) *//*| (bonus3 = percentTwentyFive) |
-     (bonus4 = percentThirtyFive) | (bonus5 = percentFortyFive) | (bonus6 = percentFifty)*//*;*/
+if (bonus1) 
+    Console.WriteLine($"You have been working {experience} years, you bonus {PERCENT_TEN}% ");
+else if (bonus2) 
+    Console.WriteLine($"You have been working {experience} years, you bonus {PERCENT_FIFTEEN}% ");
+else if (bonus3) 
+    Console.WriteLine($"You have been working {experience} years, you bonus {PERCENT_TWENTY_FIVE}% ");
+else if (bonus4)
+    Console.WriteLine($"You have been working {experience} years, you bonus {PERCENT_THIRTY_FIVE}% ");
+else if (bonus5)
+    Console.WriteLine($"You have been working {experience} years, you bonus {PERCENT_FORTY_FIVE}% ");
+else if (bonus6)
+    Console.WriteLine($"You have been working {experience} years, you bonus {PERCENT_FIFTY}% ");
+else 
+    Console.WriteLine($"You have been working {experience} years, no bonus is given ");
 
-Console.WriteLine($"You experience {experience} years, you have {bonusPercent}% bonus");
- 
+
+
+
 
 
